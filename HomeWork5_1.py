7f01adf57325246dbb8da7fffb778720730dedd7
andrsmolin <105610316+andrsmolin@users.noreply.github.com>
2022-10-14 19:40:15 +0300

    Update HomeWork5_1.py

--- HomeWork5_1.py
+++ HomeWork5_1.py
@@ -1,32 +1,100 @@
 # Создайте программу для игры в ""Крестики-нолики""
-# -Отрисовка поля
-# -создание словаря
-# -функция ходов игры
-# -функция победы
-# -проверки()
+'''
+import os
+import random
+# from typing import Dict
 
-from typing import Dict
+def desk(dct):
+    # os.system("clear")
+    print(f"   1   2  3")
+    print(f"A {dct['a']['1']} | {dct['a']['2']} | {dct['a']['3']}")
+    print(f"---------------")
+    print(f"B {dct['b']['1']} | {dct['b']['2']} | {dct['b']['3']}")
+    print(f"---------------")
+    print(f"C {dct['c']['1']} | {dct['c']['2']} | {dct['c']['3']}")
 
+def chek_inp(elem, check_list):
+    return not (check_list.count(elem))
+    
+    
+def chek_empty(fld, x, y):
+    
+    if (fld[x][y]).count(' ') == 1:
+        return True
+    else:
+        return False
+    
 
-def desk(dic):
-    a0 = f"  X1  X2  X3"
-    a1 = f"Y1 {dic['y1']['x1']} | {dic['y1']['x2']} | {dic['y1']['x3']}"
-    a1_1 = f"---------------"
-    a2 = f"Y2 {dic['y2']['x1']} | {dic['y2']['x2']} | {dic['y2']['x3']}"
-    a2_1 = f"---------------"
-    a3 = f"Y3 {dic['y3']['x1']} | {dic['y3']['x2']} | {dic['y3']['x3']}"
-    print(a0)
-    print(a1)
-    print(a1_1)
-    print(a2)
-    print(a2_1)
-    print(a3)
+def check_win(elem, fld):
+    a1 = ((fld['a']['1'] + fld['a']['2'] + fld['a']['3']).count(elem) == 3)
+    a2 = ((fld['b']['1'] + fld['b']['2'] + fld['b']['3']).count(elem) == 3)
+    a3 = ((fld['c']['1'] + fld['c']['2'] + fld['c']['3']).count(elem) == 3)
+    a4 = ((fld['a']['1'] + fld['b']['1'] + fld['c']['1']).count(elem) == 3)
+    a5 = ((fld['a']['2'] + fld['b']['2'] + fld['c']['2']).count(elem) == 3)
+    a6 = ((fld['a']['3'] + fld['b']['3'] + fld['c']['3']).count(elem) == 3)
+    a7 = ((fld['a']['1'] + fld['b']['2'] + fld['c']['3']).count(elem) == 3)
+    a8 = ((fld['a']['3'] + fld['b']['2'] + fld['c']['1']).count(elem) == 3)
+    if a1 or a2 or a3 or a4 or a5 or a6 or a7 or a8:
+        return True
+        
+    
+def move(count):
+    if count:
+        print(f"игрок {player1}, Ваш ход!: ")
+    else:
+        print(f"игрок {player2}, Ваш ход!: ")
+    while (True):
+        goriz = input("gorizont? (1,2,3): ")
+        if not chek_inp(goriz, ['1', '2', '3']):
+            break
+    while (True):
+        vert = input("vert? (a,b,c): ")
+        if not chek_inp(vert, ['a', 'b', 'c']):
+            break
+    return(vert, goriz)
 
-
-dict = {
-    'y1': {'x1': ' ', 'x2': ' ', 'x3': ' '},
-    'y2': {'x1': ' ', 'x2': ' ', 'x3': ' '},
-    'y3': {'x1': ' ', 'x2': ' ', 'x3': ' '}
+field = {
+    'a': {'1': ' ', '2': ' ', '3': ' '},
+    'b': {'1': ' ', '2': ' ', '3': ' '},
+    'c': {'1': ' ', '2': ' ', '3': ' '}
 }
+# mark = ''
+player1 = input("player1(X): ")  # имена игроков
+player2 = input("player2(0): ")
+# vert, goriz = 'a', '1'
 
-desk(dict)
+count = not random.randint(0, 1)                                          # Жеребъевка
+
+if count:
+    print(f"В результате жеребъевки первым ходит игрок {player1}(X)")      # Жеребъевка
+else:
+    print(f"В результате жеребъевки первым ходит игрок {player2}(0)")    # Жеребъевка
+
+desk(field)                                                               # Пустое поле
+
+moves = 0                                                                   # обнуляем ходы
+
+while moves <= 9 :
+    vert, goriz = move(count)
+    if count:
+        mark = 'X'
+        player = player1
+    else:
+        mark = '0'
+        player = player2
+        
+    while not chek_empty(field, vert, goriz):
+        print("is closed! Try again")
+        vert, goriz = move(count)
+    
+    field[vert][goriz] = mark
+    desk(field)    
+    if check_win(mark, field):
+        print(f"игрок {player} выиграл!")
+        break
+    elif moves == 9 :
+        print("Ничья")
+        break
+    else:
+        count = not count
+        moves += 1
