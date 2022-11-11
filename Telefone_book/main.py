80c731a832f04061b1e999a37bba34508e195172
Andrey <acord.81@mail.ru>
2022-11-11 18:46:00 +0300

    9-10 дз

--- Telefone_book/main.py
+++ Telefone_book/main.py
@@ -4,7 +4,50 @@ import modul_use_telefone_book as m_u_tb
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-my_token = "token"
+my_token = "my_token"
+
+
+async def choise(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global msg
+    msg = update.message.text
+    global user_choise
+    tel_dect = m_u_tb.return_data()
+    if user_choise == 1:
+        count = 0
+        for i in range(1, len(tel_dect)+1):
+            if tel_dect[i]['surname'].lower().find(msg.lower()) >= 0:
+                await update.message.reply_text(f'{tel_dect[i]}')
+                count += 1
+        else:
+            if count == 0:
+                await update.message.reply_text(f'Контакт не найден')
+            await update.message.reply_text(f'Выйти в основное меню? /start \n продолжить поиск (введите фамилию):')
+    if user_choise == 2:
+        new_cont = msg.split()
+        if len(new_cont) < 4:
+            new_cont = ''
+            return
+        surname = new_cont[0]
+        name = new_cont[1]
+        number_person = new_cont[2]
+        status = new_cont[3]
+        i = len(tel_dect)+1
+        tel_dect[i] = {'idd': i, 'surname': surname, 'name': name,
+                       'telefon': number_person, 'status': status}
+        m_u_tb.exp_dect(tel_dect)
+        await update.message.reply_text(f'{tel_dect[i]} \n Выйти в основное меню? /start \n продолжить (введите фамилию, имя, номер телефона и должность):')
+    if user_choise == 3:
+        idd = msg
+        if idd.isdigit() and 0 < int(idd) <= len(tel_dect):
+            await update.message.reply_text(f'информация контакта {tel_dect[int(idd)]} удалена')
+            tel_dect[int(idd)] = {'idd': idd, 'surname': 'delete',
+                                  'name': 'delete', 'telefon': '_', 'status': '_'}
+            m_u_tb.exp_dect(tel_dect)
+            await update.message.reply_text(f'Выйти в основное меню? /start \n продолжить (введите id):')
+
+        else:
+            await update.message.reply_text('повторите ввод')
+    return
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -12,80 +55,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def read(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global user_choise
+    user_choise = 1
     await update.message.reply_text(f'Введите фамилию (для возврата в основное меню - /start):')
-    # return (MessageHandler(filters.TEXT, input))
-    app.add_handler(MessageHandler(filters.TEXT, input))
-    # def read():
-#     #     await update.message.reply_text(f'Введите фамилию (для возврата в основное меню - /start):')
-#     app.add_handler(MessageHandler(filters.TEXT, input))
+    app.add_handler(MessageHandler(filters.TEXT, choise))
 
 
 async def read2(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global user_choise
+    user_choise = 2
     await update.message.reply_text(f'Введите фамилию, имя, номер телефона и должность (для возврата в основное меню - /start)')
-    app.add_handler(MessageHandler(filters.TEXT, create))
-
-    return
+    app.add_handler(MessageHandler(filters.TEXT, choise))
 
 
 async def read3(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    for i in range(0, 1):
-        await update.message.reply_text(f'Введите id (для возврата в основное меню - /start)')
-        app.add_handler(MessageHandler(filters.TEXT, delete))
-    # delete()
-    return
-
-
-async def input(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = update.message.text.lower()
-    tel_dect = m_u_tb.return_data()
-    count = 0
-    for i in range(1, len(tel_dect)+1):
-        if tel_dect[i]['surname'].lower().find(msg) >= 0:
-            await update.message.reply_text(f'{tel_dect[i]}')
-            count += 1
-    else:
-        if count == 0:
-            await update.message.reply_text(f'Контакт не найден')
-        await update.message.reply_text(f'Выйти в основное меню? /start \n продолжить поиск (введите фамилию):')
-    app = ApplicationBuilder().token(my_token).build()
-    return
-
-
-async def create(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    tel_dect = m_u_tb.return_data()
-    new_cont = update.message.text.split()
-    if len(new_cont) < 4:
-        new_cont = ''
-        return
-    surname = new_cont[0]
-    name = new_cont[1]
-    number_person = new_cont[2]
-    status = new_cont[3]
-    i = len(tel_dect)+1
-    tel_dect[i] = {'idd': i, 'surname': surname, 'name': name,
-                   'telefon': number_person, 'status': status}
-    m_u_tb.exp_dect(tel_dect)
-    await update.message.reply_text(f'{tel_dect[i]} \n /start')
-
-
-async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    idd = update.message.text
-    tel_dect = m_u_tb.return_data()
-    if idd.isdigit() and 0 < int(idd) <= len(tel_dect):
-        tel_dect[int(idd)] = {'idd': idd, 'surname': 'delete',
-                              'name': 'delete', 'telefon': '_', 'status': '_'}
-        m_u_tb.exp_dect(tel_dect)
-    else:
-        await update.message.reply_text('повторите ввод')
-        return
-
-
-def rest():
-    app = ApplicationBuilder().token(my_token).build()
-
+    global user_choise
+    user_choise = 3
+    await update.message.reply_text(f'Введите id контакта для удаления (для возврата в основное меню - /start)')
+    app.add_handler(MessageHandler(filters.TEXT, choise))
 
 app = ApplicationBuilder().token(my_token).build()
-
+user_choise = 0
 # app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
 # app.add_handler(MessageHandler(filters.TEXT, create))
@@ -98,4 +88,3 @@ app.add_handler(CommandHandler("3", read3))
 
 app.run_polling()
 msg = "0"
-# s = ""
